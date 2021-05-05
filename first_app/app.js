328fe2f37f124f38984344e331fcf7e6880e5206
BrainAxe <tanzimrizwan@gmail.com>
2021-05-06 01:08:09 +0600

    tried number, string, object, array, tuple type casting

--- first_app/app.js
+++ first_app/app.js
@@ -1,1 +1,33 @@
-console.log("Your code goes here ........");
+function add(n1, n2, showResult, phrase) {
+    var result;
+    result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5; // 5.0;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
+// Explicit way of type casting
+// let person: { name: string; age: number; hobbies: string[] } = {
+//   name: 'Tanzim Rizwan',
+//   age: 28,
+//   hobbies: ['Reading', 'Cooking']
+// };
+// Implicit way of type casting
+var person = {
+    name: 'Tanzim Rizwan',
+    age: 28,
+    hobbies: ['Reading', 'Cooking']
+};
+console.log(person.name);
+var favoriteActivities;
+favoriteActivities = ['Cooking'];
+var roles; // Tuples 
+roles = [1, 'admin'];
+roles.push(5); // Exception
--- first_app/app.ts
+++ first_app/app.ts
@@ -1,1 +1,39 @@
-console.log("Your code goes here ........");
+function add(n1:number, n2:number, showResult: boolean, phrase:string) {
+  let result:number; 
+  result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = 5 // 5.0;
+const number2 = 2.8;
+let printResult = true;
+let resultPhrase = 'Result is: ';
+
+const result = add(number1, number2, printResult, resultPhrase);
+
+// Explicit way of type casting
+// let person: { name: string; age: number; hobbies: string[] } = {
+//   name: 'Tanzim Rizwan',
+//   age: 28,
+//   hobbies: ['Reading', 'Cooking']
+// };
+
+// Implicit way of type casting
+let person = {
+  name: 'Tanzim Rizwan',
+  age: 28,
+  hobbies: ['Reading', 'Cooking']
+};
+
+console.log(person.name);
+
+let favoriteActivities: string[];
+favoriteActivities = ['Cooking'];
+
+let roles: [number, string]; // Tuples 
+roles = [1, 'admin'];
+roles.push(5) // Exception
