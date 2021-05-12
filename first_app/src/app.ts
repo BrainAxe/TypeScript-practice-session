f87a359540020ebca112f6242ee765bbea991886
BrainAxe <tanzimrizwan@gmail.com>
2021-05-12 14:35:43 +0600

    tried generic function

--- first_app/src/app.ts
+++ first_app/src/app.ts
@@ -1,1 +1,7 @@
-// code goes here
+function merge<T, U>(objA:T, objB:U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({name: 'Tanzim'}, {age: 28});
+
+console.log(mergedObj.age);
