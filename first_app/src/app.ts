0f2d15bdf26659a09250ab20af7613eec83b75cf
BrainAxe <tanzimrizwan@gmail.com>
2021-05-12 17:16:19 +0600

    implemented another generi function

--- first_app/src/app.ts
+++ first_app/src/app.ts
@@ -5,3 +5,19 @@ function merge<T extends object, U extends object>(objA:T, objB:U) {
 const mergedObj = merge({name: 'Tanzim'}, {age: 28});
 
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
