77b00e216feb5a572b57cd956d9d38874324b209
BrainAxe <tanzimrizwan@gmail.com>
2021-05-12 23:14:21 +0600

    tried keyof constrains

--- first_app/src/app.ts
+++ first_app/src/app.ts
@@ -21,3 +21,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
 }
 
 console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj:T, key:U) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({name: "Tanzim"}, "name"));
