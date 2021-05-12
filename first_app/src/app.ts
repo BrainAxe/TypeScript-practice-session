35d8dfa3a419342ffedbf38c0365333f1cbe2cc8
BrainAxe <tanzimrizwan@gmail.com>
2021-05-13 01:42:24 +0600

    tried generic classes

--- first_app/src/app.ts
+++ first_app/src/app.ts
@@ -27,3 +27,42 @@ function extractAndConvert<T extends object, U extends keyof T>(obj:T, key:U) {
 }
 
 console.log(extractAndConvert({name: "Tanzim"}, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item:T) {
+    this.data.push(item);
+  }
+
+  removeItem(item:T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItem() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Tanzim');
+textStorage.addItem('Rizwan');
+textStorage.removeItem('Tanzim');
+console.log(textStorage.getItem());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(5);
+numberStorage.addItem(51);
+numberStorage.removeItem(5);
+console.log(numberStorage.getItem());
+
+// const objectStorage = new DataStorage<object>();
+// const objOne = {name: 'tanzim'};
+// objectStorage.addItem(objOne);
+// objectStorage.addItem({name: 'rizwan'});
+// //....
+// objectStorage.removeItem(objOne);
+// console.log(objectStorage.getItem());
