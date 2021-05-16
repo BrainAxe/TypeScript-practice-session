9a5dfb87d5d5ad8b92f7b9f5fc2ba866d26e6d9c
BrainAxe <tanzimrizwan@gmail.com>
2021-05-16 15:45:26 +0600

    added getter

--- second_app/src/app.ts
+++ second_app/src/app.ts
@@ -138,6 +138,13 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
   private project: Project;
   
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
 
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
@@ -151,7 +158,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
 
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
