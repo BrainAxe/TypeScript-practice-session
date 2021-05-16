2eae0a4dc9c52c973d770aca10cbb8846562d3c6
BrainAxe <tanzimrizwan@gmail.com>
2021-05-16 12:32:24 +0600

    added project filter

--- second_app/src/app.ts
+++ second_app/src/app.ts
@@ -115,7 +115,14 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
     
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active'){
+          return prj.status === ProjectStatus.Active;
+        } else {
+          return prj.status === ProjectStatus.Finished;
+        }
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
     
@@ -125,6 +132,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
