42ab5ea1841e76c0b3c5650c48f8efca6b90c8d3
BrainAxe <tanzimrizwan@gmail.com>
2021-05-16 15:39:30 +0600

    added project Item to display

--- second_app/src/app.ts
+++ second_app/src/app.ts
@@ -134,6 +134,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
+  private project: Project;
+  
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -170,9 +192,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
   
