f2d47815e221f4539dfbd36f410833af5bf0e7bd
BrainAxe <tanzimrizwan@gmail.com>
2021-05-17 17:11:12 +0600

    restructured files and configured webpack

---
+++ second_app/src/components/base-component.ts
@@ -0,0 +1,32 @@
+// Component Base Class
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElemement?: string,
+  ) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as U;
+    if (newElemement) {
+      this.element.id = newElemement;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin':'beforeend', this.element);
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+
+}
---
+++ second_app/src/models/drag-drop.ts
@@ -0,0 +1,11 @@
+// Drag & Drop Interfaces
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+export interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  drophandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
